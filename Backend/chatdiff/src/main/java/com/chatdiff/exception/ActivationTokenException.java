00846f55529bb82d06e674a38ef424db3536dc5e
surajrawat1709 <surajrawat1709@gmail.com>
2024-07-15 14:47:04 +0530

    Added backend

---
+++ Backend/chatdiff/src/main/java/com/chatdiff/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.chatdiff.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
